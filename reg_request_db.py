f930eed3571e32b9f86f7733845ed8c2e2ffca26
Brian Gerard <bgerard@gmail.com>
2016-07-27 23:36:39 -0400

    Exceptions in place of prints; catches
    Change some prints in the reg db library to raised exceptions.
    
    Change scripts to catch the new exceptions.

--- reg_request_db.py
+++ reg_request_db.py
@@ -10,13 +10,14 @@ class RegDb():
     def __init__(self, dbfile):
         try:
             self._db = sqlite3.connect(dbfile)
+
         except sqlite3.OperationalError as oe:
-            # TODO: Make this re-raise the exception up the stack rather
-            #       than a print-and-die.
-            print("Cannot connect to db at {}: {}".format(oe))
-            sys.exit(1)
+            raise ConnectionError("Cannot connect to db at {}".format(dbfile))
+                    from oe
+
         else:
             self._cursor = self._db.cursor()
+
 
         '''Try to create the db if it doesn't already exist.'''
         try:
@@ -31,8 +32,10 @@ class RegDb():
                                 state TEXT,
                                 zipcode INTEGER,
                                 username TEXT unique)'''.format(_tablename))
+
         except sqlite3.OperationalError as oe:
             pass
+
         else:
             self._db.commit()
 
@@ -48,21 +51,31 @@ class RegDb():
                                 places = ', '.join(list('?' * new_member.field_count()))
                             ),
                         new_member.values()
-                        )
+                    )
+
         except sqlite3.IntegrityError as ie:
-            # TODO: Make this re-raise up the stack rather than printing.
-            print("Unable to insert: ", end='')
+            new_exception = "Unable to insert: "
             if 'username' in str(ie):
-                print("username {} was previously requested.</br>".format(username))
+                new_exception +=
+                    "username {} was previously requested.".format(username))
             elif 'email' in str(ie):
-                print("email address {} is associated with a previous request.</br>".format(email))
-            print()
+                new_exception +=
+                    "email address {} is associated with a previous request.".format(email))
+
+            raise RuntimeError(new_exception)
+
         else:
             self._db.commit()
 
     def delete(self, regid):
-        # TODO: set up try/catch with sqlite3.OperationalError
-        self._cursor.execute('''DELETE FROM {} WHERE regid = ?'''.format(_tablename), regid)
+        try:
+            self._cursor.execute(
+                    '''DELETE FROM {} WHERE regid = ?'''.format(_tablename),
+                    regid
+                )
+
+        except:
+            raise RuntimeError("Unable to delete request.")
 
     def print_queue(self):
         self._cursor.execute('''SELECT
@@ -70,27 +83,13 @@ class RegDb():
                     addr1, addr2, city, state, zipcode, username
                     FROM {}'''.format(_tablename))
         for user in self._cursor:
-            # Thought about making these simple positional parameters to format
-            # and then just passing '*user[:]', but the names make this a bit
-            # more self-documenting.  The internal debate rages on, though. :)
-            print('''Request Id: {reqid}</br>
-Name: {first} {last}</br>
-Current email: {email}</br>
-Street Address: {addr1}</br>
-                {addr2}</br>
-City, State, Zip: {city}, {state} {zipcode}</br>
-Preferred User ID: {userid}</br>
+            print('''Request Id: {}</br>
+Name: {} {}</br>
+Current email: {}</br>
+Street Address: {}</br>
+                {}</br>
+City, State, Zip: {}, {} {}</br>
+Preferred User ID: {}</br>
 &nbsp;</br>
-    '''.format(
-                reqid=user[0],
-                first=user[1],
-                last=user[2],
-                email=user[3],
-                addr1=user[4],
-                addr2=user[5],
-                city=user[6],
-                state=user[7],
-                zipcode=user[8],
-                userid=user[9],
-                )
+    '''.format(*user[:])
             )
